7762f6f009b6a43d54ec598565fbeaf4832bcb2f
Mate Papp <matepapp@icloud.com>
2017-09-14 22:28:42 +0200

    Removed routing.

---
+++ ThisDateInHistory/ThisDateInHistory/ViewController/TabBarControllerElement.swift
@@ -0,0 +1,9 @@
+//
+//  TabBarControllerElement.swift
+//  ThisDateInHistory
+//
+//  Created by Mate Papp on 2017. 09. 14..
+//  Copyright © 2017. matepapp. All rights reserved.
+//
+
+import Foundation
